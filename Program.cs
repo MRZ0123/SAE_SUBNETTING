302bd838ca7bb8be740eb2e215d0627553a68103
MRZ0123 <manuelmarz2004@gmail.com>
2024-02-29 14:33:54 +0100

    initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SAE_SUBNETTING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
